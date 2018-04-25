e6b849d54313dee2a95de77ebb0950fb61451452
George Salu <george.salu10@gmail.com>
2018-04-24 22:32:17 -0300

    criando od documentos (entidades do mongodb) 2

---
+++ src/main/kotlin/com/springboot/pontointeligente/documents/Lancamento.kt
@@ -0,0 +1,16 @@
+package com.springboot.pontointeligente.documents
+
+import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
+import com.springboot.pontointeligente.enums.TipoEnum
+import org.springframework.data.annotation.Id
+
+@Document
+data class Lancamento (
+	val data: Date,
+	val tipo: TipoEnum,
+	val funcionario: String,
+	val descricao: String? = "",
+	val localizacao: String? = "",
+	@Id val id: String? = null 
+)
