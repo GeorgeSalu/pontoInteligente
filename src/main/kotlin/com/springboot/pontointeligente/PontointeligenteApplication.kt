722b9c9ae2d055d899f048a0020ebabc728f5d8b
George Salu <george.salu10@gmail.com>
2018-04-29 17:00:26 -0300

    criando o controller de lancamentos - 1

--- src/main/kotlin/com/springboot/pontointeligente/PontointeligenteApplication.kt
+++ src/main/kotlin/com/springboot/pontointeligente/PontointeligenteApplication.kt
@@ -1,10 +1,42 @@
 package com.springboot.pontointeligente
 
+import com.springboot.pontointeligente.documents.Empresa
+import com.springboot.pontointeligente.documents.Funcionario
+import com.springboot.pontointeligente.enums.PerfilEnum
+import com.springboot.pontointeligente.repositories.EmpresaRepository
+import com.springboot.pontointeligente.repositories.FuncionarioRepository
+import com.springboot.pontointeligente.utils.SenhaUtils
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class PontointeligenteApplication
+class PontointeligenteApplication(val empresaRepository: EmpresaRepository,
+                                  val funcionarioRepository: FuncionarioRepository) : CommandLineRunner {
+    override fun run(vararg args: String?) {
+        empresaRepository.deleteAll()
+        funcionarioRepository.deleteAll()
+
+        val empresa: Empresa = Empresa("empresa", "232443554")
+        empresaRepository.save(empresa)
+
+        val admin: Funcionario = Funcionario("admin", "email@gmail.com.br",
+                SenhaUtils().gerarBcrypt("2233322"), "223454342",
+                PerfilEnum.ROLE_ADMIN, empresa.id!!)
+
+        val funcionario: Funcionario = Funcionario("funcionario",
+                "email@gmail.com.br", SenhaUtils().gerarBcrypt("223332"),
+                "3442323", PerfilEnum.ROLE_USUARIO, empresa.id!!)
+
+        funcionarioRepository.save(funcionario)
+
+        System.out.println("Empresa Id"+empresa.id)
+        System.out.println("Admin Id"+admin.id)
+        System.out.println("funcionario Id"+funcionario.id)
+    }
+
+
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(PontointeligenteApplication::class.java, *args)
